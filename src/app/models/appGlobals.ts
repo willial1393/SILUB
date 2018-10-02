fd5514e1bb1bca0e1c4e5cafc30c6ec838de66ee
William Vega <willial1393@gmail.com>
2018-10-01 23:04:41 -0500

    validación de login completa y falta validación de compos de usuario

--- src/app/models/appGlobals.ts
+++ src/app/models/appGlobals.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
+import {HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class AppGlobals {
   base_url = 'http://localhost/api/public/';
+  headers = new HttpHeaders({'Content-Type': 'application/json'});
 }
