59732502b629b21386b42899540a501649b01008
William Vega <willial1393@gmail.com>
2018-10-06 13:35:32 -0500

    Integración de API REST al proyecto, por lo cual el proyecto tiene que encontrarse en la carpeta htdocs de xampp y tener el servicio de mysql y apache arriba

--- src/app/models/appGlobals.ts
+++ src/app/models/appGlobals.ts
@@ -3,6 +3,6 @@ import {HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class AppGlobals {
-  base_url = 'http://localhost/api/public/';
+  base_url = 'http://localhost/api/public/silub/';
   headers = new HttpHeaders({'Content-Type': 'application/json'});
 }
