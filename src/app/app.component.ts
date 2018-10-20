8cd638c2da2b4037d64ae94e2f1a436a8abbb10b
kitsune1393 <willial1393@gmail.com>
2018-10-20 17:23:07 -0500

    Botón de ayuda con infromación terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,45 @@ import {Component} from '@angular/core';
 import swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SILUB';
-  login = '';
+    title = 'SILUB';
+    login = '';
 
-  constructor() {
-    this.login = sessionStorage.getItem('login');
-  }
+    constructor() {
+        this.login = sessionStorage.getItem('login');
+    }
 
-  cerrarSesion() {
-    swal(
-      {
-        title: '¿Desea cerrar sesión?',
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Aceptar',
-        cancelButtonText: 'Cancelar'
-      }).then((result) => {
-        if (result.value) {
-          sessionStorage.clear();
-          location.reload();
-        }
-      }
-    );
-  }
+    cerrarSesion() {
+        swal(
+            {
+                title: '¿Desea cerrar sesión?',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#999999',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Aceptar',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.value) {
+                    sessionStorage.clear();
+                    location.reload();
+                }
+            }
+        );
+    }
+
+    ayuda() {
+        swal({
+            title: 'SILUB',
+            html: 'Plataforma creada para la gestion y operación de los equipos de laboratorio de la Universidad de Boyacá' +
+                ' <br><br> <i>Desarrollado por William Vega y Leydinzoon Arenas</i>',
+            type: 'info',
+            confirmButtonColor: '#999999',
+            confirmButtonText: 'Aceptar',
+        });
+    }
 }
